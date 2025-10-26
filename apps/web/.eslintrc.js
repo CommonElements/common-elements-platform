a37456e7bf28467c18c45930c911037c1cfbf7ec
Mr.Harry <hschoeller@commonelements.com>
2025-10-26 05:15:05 -0400

    fix: update ESLint config for Vercel deployment compatibility

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ['@repo/config-eslint/next.js'],
+  extends: ['next/core-web-vitals', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 }
